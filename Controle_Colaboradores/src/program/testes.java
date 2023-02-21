9febd56d59d0be7028ebf465b87c4b9e495ffe5d
Leo <Leo@DESKTOP-PDGRHEP>
2023-02-21 15:00:38 -0300

    Adicionando esqueleto dos métodos.

--- Controle_Colaboradores/src/program/testes.java
+++ Controle_Colaboradores/src/program/testes.java
@@ -2,13 +2,9 @@ package program;
 
 public class testes {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		// TODO Auto-generated method stub
-		Membros m1 = new Membros("Leo", "123", "123", "Analista", "Rua 2", Utils.stringToDate("07/02/2002"), "leo.com", 15);
-		Membros m2 = new Membros("Leo", "123", "123", "Analista", "Rua 2", Utils.stringToDate("07/02/2002"), "leo.com", 15);
-		
-		
-		System.out.println(m2.toString());
+		Functions.menu();
 	}
 
 }
